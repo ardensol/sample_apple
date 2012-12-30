8aa409f833e9f4b13a3219bb9e326a6f77608d5f
Carlos Solorio <csolorio05@gmail.com>
2012-12-29 23:15:01 -0500

    added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static Pages" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
 		end
-		it "should hve the title 'home'" do
+		it "should have the title 'home'" do
 			visit '/static_pages/home'
 			page.should have_selector('title', 
 				:text => "Ruby on Rails Tutorial Sample App | Home")
@@ -27,6 +27,17 @@ end
 			visit '/static_pages/about'
 			page.should have_selector('title',
 				:text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+	describe "Contact Page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
