c59f017445db71e6ee5237c990209c664e9d4771
Carlos Solorio <csolorio05@gmail.com>
2013-02-02 20:45:25 -0300

    Finish Sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate)}
 
   it { should be_valid }
@@ -33,6 +34,8 @@ describe User do
   	before { @user.name = " " }
   	it { should_not be_valid }
   end
+
+
 
   describe "when email is not present" do 
   	before { @user.email = " " }
@@ -106,6 +109,10 @@ end
     specify { user_for_invalid_password.should be_false}
     end
   end
+  describe "remember_token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
 end
 
 
